9f93e8c6c4bcd101bea0735fa15b60fce9ad8cd3
Lydiaofficial <Lydiaamadichinyere@gmail.com>
2023-11-02 12:25:48 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+let passwordEl = document.getElementById("password-el")
+let fieldOne = document.getElementById("field-one")
+let fieldTwo = document.getElementById("field-two")
+
+
+passwordEl.addEventListener("click", function() {
+    let generatedPasswordOne = generateRandomPassword(15); 
+    let generatedPasswordTwo = generateRandomPassword(15); 
+
+    fieldOne.textContent = generatedPasswordOne;
+    fieldTwo.textContent = generatedPasswordTwo;
+});
+
+function generateRandomPassword(length) {
+    let password = '';
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        password += characters[randomIndex];
+    }
+    return password;
+}
